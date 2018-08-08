4dcaa33ae4cee4e3bc8652d052551dc645506ae6
Tyler <tylergross28@gmail.com>
2018-08-08 14:06:54 -0500

    npm install + vue config update for MDC

---
+++ public/vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  pwa: {
+    name: 'AVP',
+    themeColor: '#004D40',
+    msTileColor: '#ffffff'
+  },
+  css: undefined,
+
+  baseUrl: undefined,
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: false,
+  parallel: undefined,
+  css: undefined
+}
