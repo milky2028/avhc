bf4eeadfb439b077cb221c568d9659769583ef1b
Tyler Gross <tylergross28@gmail.com>
2018-08-30 20:30:21 -0500

    trying to use Sentry, but I can't get rid of this one error

--- public/src/main.ts
+++ public/src/main.ts
@@ -2,9 +2,14 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from '@/router';
 import store from '@/store';
+// import * as Raven from 'raven-js';
+// import * as RavenVue from 'raven-js/plugins/vue';
 import '@/registerServiceWorker';
 
 Vue.config.productionTip = true;
+// Raven.config('https://362776d26eaf4ef4a02da8d50efc9072@sentry.io/1271986')
+//     .addPlugin(RavenVue, Vue)
+//     .install();
 
 new Vue({
   router,
