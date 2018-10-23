d5e241bd68a22e1887c8f4c850dc5587d3a4b68f
Tyler <tylergross28@gmail.com>
2018-10-22 21:35:07 -0400

    badge structure

---
+++ public/src/components/CartBadge.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="badge-root"></div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+export default class CartBadge extends Vue {}
+</script>
+
