6e635be07f35e7a97f4335ab0f3258f89fc7cedd
Tyler <tylergross28@gmail.com>
2018-10-28 15:47:12 -0500

    cart component base

---
+++ public/src/components/CartDisplay.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="cart-root">this is the cart</div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class CartDisplay extends Vue {}
+</script>
+
