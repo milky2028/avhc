d3025d849f4496f4078a7569558202fda84dafb4
Tyler <tylergross28@gmail.com>
2018-11-03 13:04:39 -0500

    Winter plzr

--- public/src/components/CartItem.vue
+++ public/src/components/CartItem.vue
@@ -60,7 +60,7 @@ export default class CartItem extends Vue {
     private selectedQuantity = 0;
     private options = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-    beforeMount() {
+    private beforeMount() {
         this.selectedQuantity = this.cartItem.quantity!;
     }
 
