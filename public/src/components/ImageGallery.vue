667df41d769589130499065dc4a9b51783dc17da
Tyler Gross <tylergross28@gmail.com>
2019-02-21 19:28:46 -0600

    format for no reason

--- public/src/components/ImageGallery.vue
+++ public/src/components/ImageGallery.vue
@@ -1,51 +1,50 @@
 <template>
-    <div id="image-gallery-root" :style="backgroundStyles" :title="image.alt">
-        <img class="hidden-image" :src="image.src" :alt="image.alt">
-        <price-display v-if="this.activeProduct" id="price-display"></price-display>
-    </div>
+  <div id="image-gallery-root" :style="backgroundStyles" :title="image.alt">
+    <img class="hidden-image" :src="image.src" :alt="image.alt">
+    <price-display v-if="this.activeProduct" id="price-display"></price-display>
+  </div>
 </template>
 
 <style scoped lang="scss">
-  @import '../styles/theme.scss';
+@import '../styles/theme.scss';
 
+#image-gallery-root {
+  grid-area: image-gallery;
+  background-position: 50% 50%;
+  background-size: cover;
+  height: 42vh;
+  display: grid;
+  grid-template-columns: 115px 1fr 115px;
+  align-items: center;
+  grid-template-areas: 'left-arrow main right-arrow';
+}
+
+.loading-image {
+  background-position: 50% 50%;
+  background-size: cover;
+}
+
+#left-arrow {
+  grid-area: left-arrow;
+}
+
+#right-arrow {
+  grid-area: right-arrow;
+}
+
+#price-display {
+  grid-area: right-arrow;
+  align-self: end;
+  font-size: 45px;
+  margin: 0 10px 10px 0;
+}
+
+@media (min-width: 1025px) {
   #image-gallery-root {
-    grid-area: image-gallery;
-    background-position: 50% 50%;
-    background-size: cover;
-    height: 42vh;
-    display: grid;
-    grid-template-columns: 115px 1fr 115px;
-    align-items: center;
-    grid-template-areas:
-      "left-arrow main right-arrow"
+    grid-area: image;
+    height: calc(100% - 175px);
   }
-
-  .loading-image {
-    background-position: 50% 50%;
-    background-size: cover;
-  }
-
-  #left-arrow {
-    grid-area: left-arrow;
-  }
-
-  #right-arrow {
-    grid-area: right-arrow;
-  }
-
-  #price-display {
-    grid-area: right-arrow;
-    align-self: end;
-    font-size: 45px;
-    margin: 0 10px 10px 0;
-  }
-
-  @media (min-width: 1025px) {
-    #image-gallery-root {
-      grid-area: image;
-      height: calc(100% - 175px);
-    }
-  }
+}
 </style>
 
 <script lang="ts">
@@ -66,20 +65,24 @@ import Product from '@/types/Product';
   }
 })
 export default class ImageGallery extends Vue {
-public activeProduct!: Product;
-private get backgroundStyles() {
-  return {
-    backgroundImage: `url('${this.image.src}')`
-  };
-}
+  public activeProduct!: Product;
+  private get backgroundStyles() {
+    return {
+      backgroundImage: `url('${this.image.src}')`
+    };
+  }
 
-private get image() {
-  const path = this.$route.params.productName;
-  return {
-    src: require(`../assets/product-images/${path}.jpg`),
-    alt: (path === 'cbd-flower') ? '28 grams of CBD Hemp Flower' :
-    (path === 'unfiltered-cbd-joints') ? 'Unfiltered, Prerolled CBD Hemp Joints' : 'Prerolled Filtered Hemp CBD Cigarettes'
-  };
-}
+  private get image() {
+    const path = this.$route.params.productName;
+    return {
+      src: require(`../assets/product-images/${path}.jpg`),
+      alt:
+        path === 'cbd-flower'
+          ? '28 grams of CBD Hemp Flower'
+          : path === 'unfiltered-cbd-joints'
+          ? 'Unfiltered, Prerolled CBD Hemp Joints'
+          : 'Prerolled Filtered Hemp CBD Cigarettes'
+    };
+  }
 }
 </script>
