71e52f68d3e89f982d6ddee1ae67e61bcb590074
Tyler <tylergross28@gmail.com>
2019-02-26 05:16:01 +0000

    toolbar tap styles

--- public/src/components/AppToolbar.vue
+++ public/src/components/AppToolbar.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="toolbar">
-    <h1><router-link to="/flower">AVHC</router-link></h1>
+    <h1>
+      <router-link to="/flower">AVHC</router-link>
+    </h1>
     <div id="button-container">
-      <router-link to="/about" id="faq-icon" class="small-icon"><button>?</button></router-link>
+      <router-link to="/about" id="faq-icon" class="small-icon">
+        <button id="q-mark">?</button>
+      </router-link>
       <router-link id="cart-container" to="/cart">
         <div v-if="cartQuantity > 0" id="badge-root">{{ cartQuantity }}</div>
         <button id="cart-icon" class="mat-icon small-icon">shopping_cart</button>
       </router-link>
-      <router-link to="/orders"><button id="account" class="mat-icon small-icon">person</button></router-link>
+      <router-link to="/orders">
+        <button id="account" class="mat-icon small-icon">person</button>
+      </router-link>
     </div>
   </div>
 </template>
@@ -41,19 +47,18 @@ button {
   position: -webkit-sticky;
   top: 0;
   background-color: white;
-  box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
   display: flex;
   justify-content: space-between;
   flex-direction: row;
   align-items: stretch;
-  padding: 18px 26px;
+  padding: 10px 18px 10px 26px;
   z-index: 100;
 }
 
 #cart-container {
   display: grid;
-  grid-template-areas: 
-      "all"
+  grid-template-areas: 'all';
 }
 
 #cart-icon {
@@ -68,24 +73,30 @@ h1 {
   text-transform: uppercase;
 }
 
-#button-container { 
+#button-container {
   display: flex;
   align-items: center;
 }
 
 #faq-icon {
-  margin-bottom: -4px;
+  padding: 6px 8px;
   margin-right: 6px;
   font-family: $primary-font;
   font-size: 24px;
 }
 
-.small-icon {
-  padding: 0 8px;
+#q-mark {
+  margin: 0 2px;
 }
 
-#account {
-  padding-right: 0px;
+.small-icon {
+  padding: 6px;
+  margin: 4px;
+}
+
+.small-icon:hover {
+  background-color: rgba(0, 0, 0, 0.1);
+  border-radius: 50%;
 }
 </style>
 
@@ -104,5 +115,3 @@ export default class AppToolbar extends Vue {
   private cartQuantity!: number;
 }
 </script>
-
-
