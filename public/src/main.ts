1c72aea25b58a5914aba52ec3d13216be764716b
Tyler Gross <tylergross28@gmail.com>
2019-05-15 16:12:27 -0400

    production sentry, test results page done

--- public/src/main.ts
+++ public/src/main.ts
@@ -6,20 +6,13 @@ import '@/registerServiceWorker';
 import * as Sentry from '@sentry/browser';
 
 Vue.config.productionTip = true;
-Sentry.init({
-  dsn: 'https://362776d26eaf4ef4a02da8d50efc9072@sentry.io/1271986',
-  integrations: [new Sentry.Integrations.Vue({ Vue })],
-  beforeSend: (event) => {
-    const mode = process.env.NODE_ENV;
-    if (mode !== 'production') {
-      // tslint:disable-next-line
-      console.error(event.exception!.values![0]);
-      return event;
-    } else {
-      return event;
-    }
-  }
-});
+const mode = process.env.NODE_ENV;
+if (mode === 'production') {
+  Sentry.init({
+    dsn: 'https://362776d26eaf4ef4a02da8d50efc9072@sentry.io/1271986',
+    integrations: [new Sentry.Integrations.Vue({ Vue })]
+  });
+}
 
 new Vue({
   router,
--- public/src/views/TestResults.vue
+++ public/src/views/TestResults.vue
@@ -1,13 +1,17 @@
 <template>
   <container-view-with-button :hideBtn="true">
     <header-with-divider>Test Results</header-with-divider>
-      <ul>
-        <li
-          v-for="result of results"
-          :key="result.name">
-            <a class="link" :href="result.href">{{ result.name }}</a>
-          </li>
-      </ul>
+    <ul>
+      <li v-for="result of results" :key="result.name">
+        <a
+          target="_blank"
+          rel="noopener"
+          type="application/pdf"
+          class="link"
+          :href="`${publicPath}test-results-files/${result.href}.pdf`"
+        >{{ formatDate(result.date) }} | {{ result.strain }}</a>
+      </li>
+    </ul>
   </container-view-with-button>
 </template>
 
@@ -24,7 +28,13 @@ ul {
 import { Component, Vue } from 'vue-property-decorator';
 import ContainerViewWithButton from '@/components/ContainerViewWithButton.vue';
 import HeaderWithDivider from '@/components/HeaderWithDivider.vue';
+import { FormatJsDate } from '../exports/DateFunctions';
 
+interface TestResult {
+  href: string;
+  date: Date;
+  strain: string;
+}
 
 @Component({
   components: {
@@ -32,17 +42,40 @@ import HeaderWithDivider from '@/components/HeaderWithDivider.vue';
     HeaderWithDivider
   }
 })
-
 export default class TestResults extends Vue {
-  public results = [
+  public formatDate = FormatJsDate;
+  public publicPath = process.env.BASE_URL;
+  public results: TestResult[] = [
     {
-      href: 'Man',
-      name: 'Bird'
+      href: 'suver-haze',
+      date: new Date(2019, 9, 22),
+      strain: 'CBD Flower | Suzer Haze'
     },
     {
-      href: 'Man',
-      name: '5/14/2019 | CBD Flower Strain | Space Candy'
+      href: 'special-sauce',
+      date: new Date(2018, 10, 1),
+      strain: 'CBD Flower | Special Sauce'
+    },
+    {
+      href: 'lifter',
+      date: new Date(2018, 9, 22),
+      strain: 'CBD Flower | Lifter'
+    },
+    {
+      href: 'hawaiian-haze',
+      date: new Date(2018, 10, 1),
+      strain: 'CBD Flower | Hawaiin Haze'
+    },
+    {
+      href: 'elektra',
+      date: new Date(2018, 9, 22),
+      strain: 'CBD Flower | Elektra'
+    },
+    {
+      href: 'sour-space-candy',
+      date: new Date(2018, 9, 22),
+      strain: 'CBD Flower | Sour Space Candy'
     }
-  ]
+  ];
 }
 </script>
