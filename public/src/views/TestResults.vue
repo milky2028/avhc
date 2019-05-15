845d9f5886f4d4dabc91f7dbf3d4213fd5b02665
Tyler Gross <tylergross28@gmail.com>
2019-05-15 12:08:23 -0500

    test results base

--- public/src/views/TestResults.vue
+++ public/src/views/TestResults.vue
@@ -1,5 +1,48 @@
 <template>
-  <div>
-    <h1>This is a Test Results page</h1>
-  </div>
+  <container-view-with-button :hideBtn="true">
+    <header-with-divider>Test Results</header-with-divider>
+      <ul>
+        <li
+          v-for="result of results"
+          :key="result.name">
+            <a :href="result.href">{{ result.name }}</a>
+          </li>
+      </ul>
+  </container-view-with-button>
 </template>
+
+<style lang="scss" scoped>
+@import '@/styles/article.scss';
+
+ul {
+  margin-top: 26px;
+}
+</style>
+
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import ContainerViewWithButton from '@/components/ContainerViewWithButton.vue';
+import HeaderWithDivider from '@/components/HeaderWithDivider.vue';
+
+
+@Component({
+  components: {
+    ContainerViewWithButton,
+    HeaderWithDivider
+  }
+})
+
+export default class TestResults extends Vue {
+  public results = [
+    {
+      href: 'Man',
+      name: 'Bird'
+    },
+    {
+      href: 'Man',
+      name: 'Tyler'
+    }
+  ]
+}
+</script>
